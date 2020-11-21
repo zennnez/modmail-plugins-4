ad91c408fc5cf5b365a40b820d0383d9ee90281e
lohcy <71441288+lohcy7391@users.noreply.github.com>
2020-11-21 14:22:06 +0800

    Add files via upload

--- onmessageaddrole/onmessageaddrole.py
+++ onmessageaddrole/onmessageaddrole.py
@@ -1,1 +1,38 @@
-d
+import asyncio
+import re
+
+import discord
+from discord.ext import commands
+from discord.role import Role
+
+from core import checks
+from core.models import PermissionLevel
+
+class OnMessageAddRole(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(name="onmessageaddrole", aliases="omar", invoke_without_command=True)
+    @checks.has_permissions(PermissionLevel.MODERATOR)
+    async def onmessageaddrole(self, ctx, role: discord.Role, ):
+        """Assign roles to members when they type in specified channel."""
+        channelroles = []
+        
+        @omar.command(name="add")
+        @checks.has_permissions(PermissionLevel.MODERATOR)
+        async def omar_add(self, ctx, *, role: discord.Role, channel:discord.TextChannel.id):
+            channelroles[channel]=role
+
+        @commands.Cog.listener()
+        async def on_message(self, member):
+            member = context.author
+            for channel in channelroles:
+                role = channelroles.get(channel)
+                if role in member.roles:
+                    continue
+                else:
+                    member.add_role(role)
+
+
+def setup(bot):
+    bot.add_cog(OnMessageAddRole(bot))
