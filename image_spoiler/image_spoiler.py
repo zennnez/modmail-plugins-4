a29b53b77320f2a8d83e69c364ca4402d4f3eeb4
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-01-09 20:05:36 +0800

    Rewrite

--- image_spoiler/image_spoiler.py
+++ image_spoiler/image_spoiler.py
@@ -3,6 +3,7 @@ import re
 
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 from core import checks
 from core.thread import Thread
@@ -11,23 +12,50 @@ class ImageSpoilers(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def sptr(self, ctx, *, message, anonymous, s_thread_recipient):
+        #check if user is blocked
+        if s_thread_recipient.id in self.bot.blocked_users:
+            await ctx.message.add_reaction(self.config.blocked_emoji)
+            return
+
+        #if channel is DM channel
+        if ctx.channel.guild is None:
+            await ctx.message.add_reaction(self.config.sent_emoji)
+            s_guild = discord.utils.get(self.bot.guilds, guild_id=self.config.modmail_guild_id) if self.config.modmail_guild_id is not None else discord.utils.get(self.bot.guilds, guild_id=self.config.guild_id)
+            for channel in s_guild.channels:
+                if re.search(f"{str(s_thread_recipient.id)}", channel.topic):
+                    return await channel.send(content=f"(Response) Recipient: {message.content}", files=message.attachments)
+
+        #if channel is thread channel
+        else:
+            s_dm_channel = discord.utils.get(self.bot.private_channels, recipient=s_thread_recipient)
+            if anonymous is True:
+                return await s_dm_channel.send(content=f"(Response) {ctx.author.top_role}: {message.content}", files=message.attachments)
+            else:
+                return await s_dm_channel.send(content=f"({ctx.author.top_role}) {str(ctx.author)}: {message.content}", files=message.attachments)
+
+
     @commands.Cog.listener()
     @checks.thread_only()
     async def on_thread_reply(self, thread, from_mod, message, anonymous, plain):
+        s_thread_recipient = self.thread.recipient
+        if message.author.bot:
+            return
+
         #check if attachment is spoilered
         for attachment in message.attachments:
-            if attachment.filename.startswith("SPOILER_"):
-                return await ctx.invoke(self.bot.get_command('pareply'), msg=message.content) if anonymous is True else await ctx.invoke(self.bot.get_command('preply'), msg=message.content)
+            if attachment.is_spoiler()
+                return await sptr(message=message, anonymous-anonymous)
         
         #check if links are spoilered
         #links = re.findall("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$", message.content)
         #for link in links:
         #if re.search("SPOILER_", link) is True:
-        #return await ctx.invoke(self.bot.get_command('pareply'), msg=message.content) if anonymous is True else await ctx.invoke(self.bot.get_command('preply'), msg=message.content)
+        #return await sptr(message=message, anonymous-anonymous)
 
         #check if message contains "SPOILER_"
         if re.search("SPOILER_", message.content):
-            return await ctx.invoke(self.bot.get_command('pareply'), msg=message.content) if anonymous is True else await ctx.invoke(self.bot.get_command('preply'), msg=message.content)
+            return await sptr(message=message, anonymous=anonymous, s_thread_recipient=s_thread_recipient)
         
 def setup(bot):
     bot.add_cog(ImageSpoilers(bot))
