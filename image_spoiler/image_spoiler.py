ac2c6c4f93efcc251d3d510c922201a20a7678e8
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-01-09 12:14:06 +0800

    Add files via upload

--- image_spoiler/image_spoiler.py
+++ image_spoiler/image_spoiler.py
@@ -1,1 +1,37 @@
+import asyncio
+import re
 
+import discord
+from discord.ext import commands
+
+from core import checks
+
+
+class ImageSpoilers(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    @checks.thread_only()
+    async def on_message(self, ctx):
+        msg = ctx.message
+        global lic
+        lic = 0
+        if not ctx.author.bot:
+            for attachment in msg.attachments:
+                if attachment.filename.startswith("SPOILER_"):
+                    lic += 1
+            links = re.findall(r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", msg.content)
+            for key in links:
+                spoiler = re.search("SPOILER_",str(key))
+                if spoiler is not None:
+                    lic += 1
+        
+        if lic > 0:
+            return await with ctx.typing():
+                await ctx.thread.reply(msg, plain=True)o
+
+                
+
+def setup(bot):
+    bot.add_cog(ImageSpoilers(bot))
