757af027ceebe0e1c2b599e53c64ef1f83bf2fe6
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-01-09 18:00:58 +0800

    Rewrite

--- image_spoiler/image_spoiler.py
+++ image_spoiler/image_spoiler.py
@@ -14,21 +14,20 @@ class ImageSpoilers(commands.Cog):
     @commands.Cog.listener()
     @checks.thread_only()
     async def on_thread_reply(self, thread, from_mod, message, anonymous, plain):
-        global lic
-        lic = 0
-        if not message.author.bot:
-            for attachment in message.attachments:
-                if attachment.filename.startswith("SPOILER_"):
-                    lic += 1
-            links = re.findall(r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", message.content)
-            for key in links:
-                spoiler = re.search("SPOILER_",str(key))
-                if spoiler is not None:
-                    lic += 1
+        #check if attachment is spoilered
+        for attachment in message.attachments:
+            if attachment.filename.startswith("SPOILER_"):
+                return await ctx.invoke(self.bot.get_command('pareply'), msg=message.content) if anonymous is True else await ctx.invoke(self.bot.get_command('preply'), msg=message.content)
         
-        if lic > 0:
-            return await thread.reply(message, plain=True)
-                
+        #check if links are spoilered
+        #links = re.findall("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$", message.content)
+        #for link in links:
+        #if re.search("SPOILER_", link) is True:
+        #return await ctx.invoke(self.bot.get_command('pareply'), msg=message.content) if anonymous is True else await ctx.invoke(self.bot.get_command('preply'), msg=message.content)
 
+        #check if message contains "SPOILER_"
+        if re.search("SPOILER_", message.content):
+            return await ctx.invoke(self.bot.get_command('pareply'), msg=message.content) if anonymous is True else await ctx.invoke(self.bot.get_command('preply'), msg=message.content)
+        
 def setup(bot):
     bot.add_cog(ImageSpoilers(bot))
