fe66542fa12b4447e842d78fe3e1d30d0f6d4058
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-02-19 03:39:33 +0800

    Add files via upload

--- messagepin/messagepin.py
+++ messagepin/messagepin.py
@@ -1,1 +1,18 @@
+import asyncio
 
+import discord
+from discord.ext import commands
+
+class MessagePin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def pin(self, ctx, *, channel=discord.TextChannel):
+        channel=channel
+        PML = await for channel.history(limit=1).flatten()
+        PM = PML[0]
+        return await PM.pin()
+
+def setup(bot):
+    bot.add_cog(MessagePin(bot))
