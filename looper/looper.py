6ad89a2ff8ceedb6eedcdff1d3a10c2b417c7c05
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-03-07 15:03:56 +0800

    Create looper.py

---
+++ looper/looper.py
@@ -0,0 +1,20 @@
+import asynchio
+
+import discord
+from discord.ext import commands
+
+class Looper(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def loop(self, ctx, *, role: discord.Role):
+        await for m in ctx.members:
+            for r in m.roles:
+                break if r is role else continue
+            else:
+                await ctx.send(content=f"<@{m.id}>")
+        return await ctx.send(content="Loop done!")
+
+def setup(bot):
+    bot.add_cog(Looper(bot))
