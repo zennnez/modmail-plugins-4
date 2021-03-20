a55cc5c62cbc4576a9293c5079cae35a1649389e
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-03-20 21:12:05 +0800

    Add files via upload

---
+++ msg_send/msg_send.py
@@ -0,0 +1,31 @@
+import asyncio
+
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+class MsgSend(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(invoke_without_command = True)
+    @checks.has_permission(PermissionLevel.OWNER)
+    async def msgsend(self, ctx: commands.Context):
+        """
+        Lets Modmail sends messge to the server.
+        """
+        await ctx.send_help(ctx.command)
+
+    @msgsend.command(name="add")
+    @checks.has_permission(PermissionLevel.OWNER)
+    async def msgsend_add(self, ctx, channel: discord.TextChannel, *, message: str):
+        """
+        Sends a message to a channel through Modmail.
+        """
+
+        await channel.send(content=message)
+
+def setup(bot):
+    bot.add_cog(MsgSend(bot))
