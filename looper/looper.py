7a3eb21fd204abe6dfc9b5a9ef037c7680036d55
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-04-03 12:13:49 +0800

    Update looper.py

--- looper/looper.py
+++ looper/looper.py
@@ -11,26 +11,28 @@ class Looper(commands.Cog):
     async def loop(self, ctx, arg: bool, *, role: discord.Role):
         """
         To list members that have or don't have a specified role.
-        False list members without the role, while True lists members with the role.
+        0 list members without the role, while 1 lists members with the role.
         Command is case-sensitive.
         """
+        x = 0
         if arg is False:
             for m in ctx.guild.members:
                 if role in m.roles:
                     continue 
                 else:
+                    x += 1
                     await ctx.send(content=f"<@{m.id}>")
-            return await ctx.send(content="Loop done!")
+            return await ctx.send(content=f"Loop done! {x} members do not have {role.name}.")
         elif arg is True:
             for m in ctx.guild.members:
                 if role not in m.roles:
                     continue 
                 else:
+                    x += 1
                     await ctx.send(content=f"<@{m.id}>")
-            return await ctx.send(content="Loop done!")
-            return await ctx.send(content="Loop done!")
+            return await ctx.send(content=f"Loop done! {x} members have {role.name}.")
         else:
-            return await ctx.send(content="Argument must be either True or False, case-sensitive.")
+            return await ctx.send(content="Argument must be either True or False. Command is case-sensitive.")
 
 def setup(bot):
     bot.add_cog(Looper(bot))
