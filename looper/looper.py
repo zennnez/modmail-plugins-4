1e2c52acfd8e209d8d3790e8e2490601155a518f
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-04-03 12:00:26 +0800

    Update looper.py

--- looper/looper.py
+++ looper/looper.py
@@ -8,14 +8,21 @@ class Looper(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def loop(self, ctx, *, role: discord.Role):
-        for m in ctx.guild.members:
-            for r in m.roles:
-                if r is role:
-                    break
-            else:
-                await ctx.send(content=f"<@{m.id}>")
-        return await ctx.send(content="Loop done!")
+    async def loop(self, ctx, arg = int, *, role: discord.Role):
+        """
+        To list members that have or don't have a specified role.
+        0 list members without the role, while 1 lists members with the role.
+        """
+        if arg = 0:
+            for m in ctx.members:
+                continue if role in m.roles else await ctx.send(content=f"<@{m.id}>")
+            return await ctx.send(content="Loop done!")
+        elif if arg = 1:
+            for m in ctx.members:
+                continue if role not in m.roles else await ctx.send(content=f"<@{m.id}>")
+            return await ctx.send(content="Loop done!")
+        else:
+            return await ctx.send(content="Argument must be either 0 or 1.")
 
 def setup(bot):
     bot.add_cog(Looper(bot))
