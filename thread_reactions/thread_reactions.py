94a23e1dbccd0bf436eb42fa079b621f462e0769
lohcy <71441288+lohcy7391@users.noreply.github.com>
2021-09-06 16:16:57 +0800

    Add files via upload

--- thread_reactions/thread_reactions.py
+++ thread_reactions/thread_reactions.py
@@ -74,7 +74,7 @@ class ThreadReactions(commands.Cog):
 
         def emojiCheck(arg):
             if type(arg) == str:
-                arg = emoji.demojize(arg) if arg in UNICODE_EMOJI["en"] else None
+                arg = emoji.demojize(arg) if arg in emoji.UNICODE_EMOJI["en"] else None
             else:
                 arg = arg.name if arg.id is None else str(arg.id)
             return arg
@@ -112,7 +112,7 @@ class ThreadReactions(commands.Cog):
 
         def emojiCheck(arg):
             if type(arg) == str:
-                arg = emoji.demojize(arg) if arg in UNICODE_EMOJI["en"] else None
+                arg = emoji.demojize(arg) if arg in emoji.UNICODE_EMOJI["en"] else None
             else:
                 arg = arg.name if arg.id is None else str(arg.id)
             return arg
